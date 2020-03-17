00c4241d064067713d15361ddffd585797b1a5fc
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2020-03-17 10:37:46 -0500

    added try catch for choose grazing field

--- src/Actions/ChooseGrazingField.cs
+++ src/Actions/ChooseGrazingField.cs
@@ -7,87 +7,88 @@ using Trestlebridge.Models.Animals;
 
 namespace Trestlebridge.Actions
 {
-  public class ChooseGrazingField
-  {
-    public static void CollectInput(Farm farm, IGrazing animal)
+    public class ChooseGrazingField
     {
-      Utils.Clear();
-
-      for (int i = 0; i < farm.GrazingFields.Count; i++)
-      {
-
-        if (farm.GrazingFields[i].animalCount != farm.GrazingFields[i].Capacity)
+        public static void CollectInput(Farm farm, IGrazing animal)
         {
-          var cowCount = 0;
-          var pigCount = 0;
-          var goatCount = 0;
-          var ostrichCount = 0;
-          var sheepCount = 0;
+            Utils.Clear();
 
-          Console.WriteLine($"{i + 1}. {farm.GrazingFields[i].ToString()} Max Capacity: {farm.GrazingFields[i].Capacity}");
+            for (int i = 0; i < farm.GrazingFields.Count; i++)
+            {
 
-          cowCount = farm.GrazingFields[i]._animals
-              .Where(monkeyButt => monkeyButt.Type == "Cow")
-              .Count();
-          Console.WriteLine($"Cow: {cowCount}");
+                if (farm.GrazingFields[i].animalCount != farm.GrazingFields[i].Capacity)
+                {
+                    var cowCount = 0;
+                    var pigCount = 0;
+                    var goatCount = 0;
+                    var ostrichCount = 0;
+                    var sheepCount = 0;
 
-          pigCount = farm.GrazingFields[i]._animals
-              .Where(monkeyButt => monkeyButt.Type == "Pig")
-              .Count();
-          Console.WriteLine($"Pig: {pigCount}");
+                    Console.WriteLine($"{i + 1}. {farm.GrazingFields[i].ToString()} Max Capacity: {farm.GrazingFields[i].Capacity}");
 
-          goatCount = farm.GrazingFields[i]._animals
-              .Where(monkeyButt => monkeyButt.Type == "Goat")
-              .Count();
-          Console.WriteLine($"Goat: {goatCount}");
+                    cowCount = farm.GrazingFields[i]._animals
+                        .Where(monkeyButt => monkeyButt.Type == "Cow")
+                        .Count();
+                    Console.WriteLine($"Cow: {cowCount}");
 
-          ostrichCount = farm.GrazingFields[i]._animals
-              .Where(monkeyButt => monkeyButt.Type == "Ostrich")
-              .Count();
-          Console.WriteLine($"Ostrich: {ostrichCount}");
+                    pigCount = farm.GrazingFields[i]._animals
+                        .Where(monkeyButt => monkeyButt.Type == "Pig")
+                        .Count();
+                    Console.WriteLine($"Pig: {pigCount}");
 
-          sheepCount = farm.GrazingFields[i]._animals
-              .Where(monkeyButt => monkeyButt.Type == "Sheep")
-              .Count();
-          Console.WriteLine($"Sheep: {sheepCount}\n");
+                    goatCount = farm.GrazingFields[i]._animals
+                        .Where(monkeyButt => monkeyButt.Type == "Goat")
+                        .Count();
+                    Console.WriteLine($"Goat: {goatCount}");
+
+                    ostrichCount = farm.GrazingFields[i]._animals
+                        .Where(monkeyButt => monkeyButt.Type == "Ostrich")
+                        .Count();
+                    Console.WriteLine($"Ostrich: {ostrichCount}");
+
+                    sheepCount = farm.GrazingFields[i]._animals
+                        .Where(monkeyButt => monkeyButt.Type == "Sheep")
+                        .Count();
+                    Console.WriteLine($"Sheep: {sheepCount}\n");
+                }
+
+            }
+
+            //
+            Console.WriteLine();
+            // How can I output the type of animal chosen here?
+
+            Console.WriteLine($"Place the animal where?");
+            Console.Write("> ");
+            string choiceChecker = Console.ReadLine();
+            try
+            {
+                int choice = Int32.Parse(choiceChecker) - 1;
+                if (choice < farm.GrazingFields.Count)
+                {
+                    farm.GrazingFields[choice].AddResource(animal);
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid field. You may need to create one!");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"This is not a valid choice. Try again.");
+                Console.ReadLine();
+            }
+
+
+
+
+
+            /*
+                Couldn't get this to work. Can you?
+                Stretch goal. Only if the app is fully functional.
+             */
+            // farm.PurchaseResource<IGrazing>(animal, choice);
         }
-
-      }
-
-      //
-      Console.WriteLine();
-      // How can I output the type of animal chosen here?
-
-      Console.WriteLine($"Place the animal where?");
-      Console.Write("> ");
-      int choice = Int32.Parse(Console.ReadLine()) - 1;
-
-      //   try
-      //   {
-      //     int choice = Int32.Parse(Console.ReadLine()) - 1;
-      //   }
-      //   catch (Exception)
-      //   {
-
-      //   }
-
-      if (choice < farm.GrazingFields.Count)
-      {
-        farm.GrazingFields[choice].AddResource(animal);
-      }
-      else
-      {
-        Console.WriteLine("Not a valid field. You may need to create one!");
-        Console.ReadLine();
-      }
-
-
-
-      /*
-          Couldn't get this to work. Can you?
-          Stretch goal. Only if the app is fully functional.
-       */
-      // farm.PurchaseResource<IGrazing>(animal, choice);
     }
-  }
 }
